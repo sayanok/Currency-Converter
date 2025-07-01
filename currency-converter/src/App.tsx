111037920d37d0d8e8122c5825747686658259c8
sayanok <rng.jhn.osca@gmail.com>
2025-07-01 12:39:49 +0900

    大体の流れ

--- currency-converter/src/App.tsx
+++ currency-converter/src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [amount, setAmount] = useState(String); //直す
+  const [converted, setConverted] = useState(1);
+  const [currencyBeforeConversion, setCurrencyBeforeConversion] = useState(String);
+  const [currencyAfterConversion, setCurrencyAfterConversion] = useState(String);
+
+  function clickHandler() {
+    convert();
+  }
+
+  function convert() {
+    console.log(currencyAfterConversion);
+    fetch("https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/" + { currencyBeforeConversion }) //APIKEY発行する
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("リクエストエラー:", error);
+      });
+
+    // 変換後のレートをセットする
+    // amountにレートをかける
+
+    setConverted(111);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <form>
+        <input
+          type="number"
+          onChange={(e) => {
+            setAmount(e.target.value);
+          }}
+        ></input>
+        <select
+          name="currency"
+          id="currency-select"
+          onChange={(e) => {
+            setCurrencyBeforeConversion(e.target.value);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <option value="yen">yen</option>
+          <option value="usd">usd</option>
+          <option value="eur">eur</option>
+        </select>
+        →<div>{converted}</div>
+        <select
+          name="currency"
+          id="currency-select"
+          onChange={(e) => {
+            setCurrencyAfterConversion(e.target.value);
+          }}
+        >
+          <option value="yen">yen</option>
+          <option value="usd">usd</option>
+          <option value="eur">eur</option>
+        </select>
+        <button type="button" onClick={() => clickHandler()}>
+          変換する
+        </button>
+      </form>
+    </>
   );
-}
-
+};
 export default App;
